87abfa8bbe917f0f190a0cac4effd42599bdde03
Shuvam Mazumder <shuvammaz94@gmail.com>
2022-07-14 22:23:26 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import News from './components/News';
 
 function App() {
+  const [news, setNews] = useState([]);
+  useEffect ( () =>{
+    fetch('https://newsapi.org/v2/everything?q=tesla&from=2022-06-12&sortBy=publishedAt&apiKey=6fbb1a557c9f40859632c43f246a1c4b')
+    .then(res => res.json())
+    .then(data=>setNews(data.articles));
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant='secondary'>button</Button>
+      <Row xs={1} md={3} className="g-4">
+      {
+        news.map(nw => <News news={nw}></News>)
+      }
+      
+    </Row>
     </div>
   );
 }
---
+++ src/components/News.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+
+const News = (props) => {
+    const {title,description,urlToImage}=props.news;
+  return (
+    <div>
+        <Col>
+          <Card>
+            <Card.Img variant="top" src={urlToImage} />
+            <Card.Body>
+              <Card.Title>{title}</Card.Title>
+              <Card.Text>
+                {description}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+    </div>
+  )
+}
+
+export default News;
+
